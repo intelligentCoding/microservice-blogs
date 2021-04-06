b295c8d4e56ce3bb1349ed7f966d669a43053de7
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-04-05 22:51:25 -0600

    moderated services finished

--- comments/index.js
+++ comments/index.js
@@ -22,7 +22,7 @@ app.post("/posts/:id/comments", async (req, res) => {
     console.log(content);
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: "pending" });
 
     commentsByPostId[req.params.id] = comments;
 
@@ -32,6 +32,7 @@ app.post("/posts/:id/comments", async (req, res) => {
         id: commentId,
         content,
         postId: req.params.id,
+        status: "pending",
       },
     });
 
@@ -39,8 +40,27 @@ app.post("/posts/:id/comments", async (req, res) => {
   }
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
   res.send({});
 });
 
